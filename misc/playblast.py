041c8821c5bd20e069d9484ce05873e9353ce607
Morten Andersen <mortentoo@users.noreply.github.com>
2017-06-07 09:24:50 +0200

    Update playblast.py

--- misc/playblast.py
+++ misc/playblast.py
@@ -32,5 +32,5 @@ filename = movieDir + filename[:-1] + ".mov"
 aPlayBackSliderPython = maya.mel.eval('$tmpVar=$gPlayBackSlider')
 sound = pm.timeControl(aPlayBackSliderPython, q=True, sound=True)
 
-# playblast!
+# playblast! 
 pm.animation.playblast(filename=filename, format="qt", compression="H.264", forceOverwrite=True, sequenceTime=False, clearCache=True, showOrnaments=False, offScreen=True, viewer=True, percent=100, quality=100, widthHeight=resolution, sound=sound)

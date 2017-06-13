94831b5b5d2b5c55f0d8875557e6d9276b1404c2
Morten Andersen <mortentoo@users.noreply.github.com>
2017-06-13 09:18:00 +0200

    Update playblast.py

--- misc/playblast.py
+++ misc/playblast.py
@@ -15,46 +15,54 @@ import re
 import os
 
 
-# default paths
-filename = os.path.splitext(os.path.basename(pm.system.sceneName()))[0]
-movieDir = pm.workspace.fileRules['movie'] + "/"
-movieDir.replace('\\', '/')
-
-if modFileName:
-    # Regex example:
-    # Filename in the format "/my/path/sh_0010_ANI_workshop_0010.ma"
-    # is converted to "sh0010"
-    pattern = re.compile('(?!\/)(.*?)_?([0-9]+)_(.*?)_')
-    match = re.match(pattern, filename)
-    filename = match.group(1) + match.group(2)
-
-
-# assemble full path and filename
-filename = movieDir + filename + ".mov"
-
-
-# get active sound in time slider
-aPlayBackSliderPython = maya.mel.eval('$tmpVar=$gPlayBackSlider')
-sound = pm.timeControl(aPlayBackSliderPython, q=True, sound=True)
-
-
-# disable resolution gate
+# get camera shape node
 view = OpenMayaUI.M3dView.active3dView()
 cam = OpenMaya.MDagPath()
 view.getCamera(cam)
 camShape = cam.partialPathName()
 
-resGateEnabled = pm.getAttr(camShape + ".displayResolution")
-overscan = pm.getAttr(camShape + ".overscan")
 
-pm.setAttr(camShape + ".displayResolution", 1)
-pm.setAttr(camShape + ".overscan", 1)
-
-
-# playblast! 
-pm.animation.playblast(filename=filename, format="qt", compression="H.264", forceOverwrite=True, sequenceTime=False, clearCache=True, showOrnaments=False, offScreen=True, viewer=True, percent=100, quality=100, widthHeight=resolution, sound=sound)
+# prompt for postfix
+message = "Playblast camera:\n" + camShape + "\n\nEnter optional postfix"
+result = pm.promptDialog(title="Playblast", message=message, button=["Playblast","Cancel"], defaultButton="Playblast", cancelButton="Cancel", dismissString="Cancel")
 
 
-# restore gate
-pm.setAttr(camShape + ".displayResolution", resGateEnabled)
-pm.setAttr(camShape + ".overscan", overscan)
+if result == "Playblast":
+    postfix = pm.promptDialog(q=True, text=True)
+
+    # default paths
+    filename = os.path.splitext(os.path.basename(pm.system.sceneName()))[0]
+    movieDir = pm.workspace.fileRules['movie'] + "/"
+    movieDir.replace('\\', '/')
+
+    if modFileName:
+        # Regex example:
+        # Filename in the format "/my/path/sh_0010_ANI_workshop_0010.ma"
+        # is converted to "sh0010_ANI"
+        pattern = re.compile('(?!\/)(.*?)_?([0-9]+)_(.*?)_')
+        match = re.match(pattern, filename)
+        filename = match.group(1) + match.group(2)
+
+
+    # get active sound in time slider
+    aPlayBackSliderPython = maya.mel.eval('$tmpVar=$gPlayBackSlider')
+    sound = pm.timeControl(aPlayBackSliderPython, q=True, sound=True)
+
+
+    # assemble full path and filename
+    filename = movieDir + filename + postfix + ".mov"
+
+    # disable resolution gate
+    resGateEnabled = pm.getAttr(camShape + ".displayResolution")
+    overscan = pm.getAttr(camShape + ".overscan")
+    pm.setAttr(camShape + ".displayResolution", 1)
+    pm.setAttr(camShape + ".overscan", 1)
+
+
+    # playblast! 
+    pm.animation.playblast(filename=filename, format="qt", compression="H.264", forceOverwrite=True, sequenceTime=False, clearCache=True, showOrnaments=False, offScreen=True, viewer=True, percent=100, quality=100, widthHeight=resolution, sound=sound)
+
+
+    # restore gate
+    pm.setAttr(camShape + ".displayResolution", resGateEnabled)
+    pm.setAttr(camShape + ".overscan", overscan)

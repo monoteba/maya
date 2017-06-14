37a83e015eff56f0e3518dbd456f46d974b7c39c
Morten Andersen <mortentoo@users.noreply.github.com>
2017-06-14 15:37:17 +0200

    Update playblast.py

--- misc/playblast.py
+++ misc/playblast.py
@@ -15,54 +15,58 @@ import re
 import os
 
 
-# get camera shape node
+# get camera name
 view = OpenMayaUI.M3dView.active3dView()
-cam = OpenMaya.MDagPath()
-view.getCamera(cam)
-camShape = cam.partialPathName()
+camPath = OpenMaya.MDagPath()
+view.getCamera(camPath)  # returns camera shape node
+camShapeName = camPath.partialPathName()
+
+cam = camPath.transform()  # returns MObject
+OpenMaya.MDagPath.getAPathTo(cam, camPath)
+camName = camPath.partialPathName()
+
+
+ # default paths
+filename = os.path.splitext(os.path.basename(pm.system.sceneName()))[0]
+movieDir = pm.workspace.fileRules['movie'] + "/"
+movieDir.replace('\\', '/')
+
+if modFileName:
+    # Regex example:
+    # Filename in the format "/my/path/sh_0010_ANI_workshop_0010.ma"
+    # is converted to "sh0010_ANI"
+    pattern = re.compile('(?!\/)(.*?)_?([0-9]+)_(.*?)_')
+    match = re.match(pattern, filename)
+    filename = match.group(1) + match.group(2)
 
 
 # prompt for postfix
-message = "Playblast camera:\n" + camShape + "\n\nEnter optional postfix"
+message = "Playblast camera:\n" + camName + "\n\nOutput name:\n" + filename + "\n\nFilename postfix (optional):"
 result = pm.promptDialog(title="Playblast", message=message, button=["Playblast","Cancel"], defaultButton="Playblast", cancelButton="Cancel", dismissString="Cancel")
 
 
 if result == "Playblast":
-	postfix = pm.promptDialog(q=True, text=True)
+    postfix = pm.promptDialog(q=True, text=True)
 
-	# default paths
-	filename = os.path.splitext(os.path.basename(pm.system.sceneName()))[0]
-	movieDir = pm.workspace.fileRules['movie'] + "/"
-	movieDir.replace('\\', '/') 
-
-	if modFileName:
-		# Regex example:
-		# Filename in the format "/my/path/sh_0010_ANI_workshop_0010.ma"
-		# is converted to "sh0010_ANI"
-		pattern = re.compile('(?!\/)(.*?)_?([0-9]+)_(.*?)_')
-		match = re.match(pattern, filename)
-		filename = match.group(1) + match.group(2)
+    # get active sound in time slider
+    aPlayBackSliderPython = maya.mel.eval('$tmpVar=$gPlayBackSlider')
+    sound = pm.timeControl(aPlayBackSliderPython, q=True, sound=True)
 
 
-	# get active sound in time slider
-	aPlayBackSliderPython = maya.mel.eval('$tmpVar=$gPlayBackSlider')
-	sound = pm.timeControl(aPlayBackSliderPython, q=True, sound=True)
+    # assemble full path and filename
+    filename = movieDir + filename + postfix + ".mov"
+
+    # disable resolution gate
+    resGateEnabled = pm.getAttr(camShapeName + ".displayResolution")
+    overscan = pm.getAttr(camShapeName + ".overscan")
+    pm.setAttr(camShapeName + ".displayResolution", 1)
+    pm.setAttr(camShapeName + ".overscan", 1)
 
 
-	# assemble full path and filename
-	filename = movieDir + filename + postfix + ".mov"
-
-	# disable resolution gate
-	resGateEnabled = pm.getAttr(camShape + ".displayResolution")
-	overscan = pm.getAttr(camShape + ".overscan")
-	pm.setAttr(camShape + ".displayResolution", 1)
-	pm.setAttr(camShape + ".overscan", 1)
+    # playblast! 
+    pm.animation.playblast(filename=filename, format="qt", compression="H.264", forceOverwrite=True, sequenceTime=False, clearCache=True, showOrnaments=False, offScreen=True, viewer=True, percent=100, quality=100, widthHeight=resolution, sound=sound)
 
 
-	# playblast! 
-	pm.animation.playblast(filename=filename, format="qt", compression="H.264", forceOverwrite=True, sequenceTime=False, clearCache=True, showOrnaments=False, offScreen=True, viewer=True, percent=100, quality=100, widthHeight=resolution, sound=sound)
-
-
-	# restore gate
-	pm.setAttr(camShape + ".displayResolution", resGateEnabled)
-	pm.setAttr(camShape + ".overscan", overscan)
+    # restore gate
+    pm.setAttr(camShapeName + ".displayResolution", resGateEnabled)
+    pm.setAttr(camShapeName + ".overscan", overscan)

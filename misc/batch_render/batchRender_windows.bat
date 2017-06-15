4a4581ed58c349ddd446cc930952ba02bbc88a79
Morten Andersen <mortentoo@users.noreply.github.com>
2017-06-15 11:32:37 +0200

    Delete batchRender_windows.bat

--- misc/batch_render/batchRender_windows.bat
+++
@@ -1,7 +0,0 @@
-# Flags
-# -r  Renderer (sw = software, hw = hardware, hw2 = hardware 2.0, vr = vector)
-# -v  Verbose level
-# -s  Start frame
-# -e  End frame
-# -rd Render Directory
-"C:\Program Files\Autodesk\Maya2016\bin\Render.exe" -r sw -v 5 -s 1 -e 50 -rd "C:\path\to\output.exr" "C:\path\to\file.ma"

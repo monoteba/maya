8d37bdf9d17fd65f4b1e7d28e8f3458652f63c4f
Morten Andersen <mortentoo@users.noreply.github.com>
2017-06-30 11:34:26 +0200

    Update playblast.py

--- snippets/animation/playblast.py
+++ snippets/animation/playblast.py
@@ -34,7 +34,7 @@ camName = camPath.partialPathName()
 # prompt for postfix
 message = "Playblast camera:\n" + camName + "\n\nFilename:"
 
-filneame = ""
+filename = ""
 try:
     filename = pm.system.fileInfo['playblastFilename']
 except:
@@ -44,9 +44,11 @@ if filename is "":
     # Regex example:
     # Filename in the format "/my/path/sh_0010_ANI_workshop_0010.ma"
     # is converted to "sh0010"
+    filename = os.path.splitext(os.path.basename(pm.system.sceneName()))[0]
     pattern = re.compile('(?!\/)(.*?)_?([0-9]+)_(.*?)_')
     match = re.match(pattern, filename)
     filename = match.group(1) + match.group(2)
+
     
 result = pm.promptDialog(title="Playblast", message=message, button=["Playblast","Cancel"], defaultButton="Playblast", cancelButton="Cancel", dismissString="Cancel", text=filename)
 

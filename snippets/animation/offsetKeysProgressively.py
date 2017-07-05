bbdf47255e520ecfb792cf7bdad1e6eea5d9ea59
Morten Andersen <mortentoo@users.noreply.github.com>
2017-07-05 16:01:20 +0200

    Update offsetKeysProgressively.py

--- snippets/animation/offsetKeysProgressively.py
+++ snippets/animation/offsetKeysProgressively.py
@@ -2,19 +2,23 @@ import pymel.core as pm
 from random import randint
 
 
-def offsetKeys(offsetMin, offsetMax):
+def offsetKeys(offsetMin, offsetMax, moveFirst=False):
 	"""
-	Offsets each object in the selection progressively, meaning the first object is offset a little, the second more, and third even more. Offset is randomized between offsetMin and offsetMax.
+	Offsets each object in the selection progressively in selected order, meaning the first object is offset a little, the second more, and third even more. Offset is randomized between offsetMin and offsetMax.
 	:param offsetMin: minimum offset
 	:param offsetMax: maximum offset
 	"""
 	sl = pm.ls(orderedSelection=True)
 	lastOffset = 0
 
+	# remove first item
+	if moveFirst is False:
+		del sl[0]
+
 	for o in sl:
 		offset = randint(offsetMin, offsetMax) + lastOffset
-		lastOffset += offset
-		pm.keyframe(o, e=True, animation='objects', time=(":"), relative=True, timeChange=offset)
+		lastOffset = offset
+		pm.keyframe(o, e=True, time=(":"), relative=True, timeChange=offset)
 
 
-offsetKeys(1,6)
+offsetKeys(0, 2, False)

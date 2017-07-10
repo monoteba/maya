ced16f940ac79880556e9f4699704d2557ad71ed
Morten Andersen <mortentoo@users.noreply.github.com>
2017-07-10 10:45:20 +0200

    Create copyChildrenTransformMatrix.py

---
+++ snippets/utilities/copyChildrenTransformMatrix.py
@@ -0,0 +1,34 @@
+'''
+Copies the transform matrix of children in one group to the children of another group.
+
+Example, selecting groupA and then groupB, will make sphere1 have the same transform as cube1, sphere2 the same as cube2 etc.
+groupA
+	- cube1
+	- cube2
+	- cube3
+
+groupB
+	- sphere1
+	- sphere2
+	- sphere3
+'''
+
+import pymel.core as pm
+
+
+sl = pm.ls(orderedSelection=True)
+
+if len(sl) != 2:
+    print "Must select exactly two groups"
+    
+else:
+    listA = listA = pm.listRelatives(sl[0], children=True, typ="transform")
+    listB = listA = pm.listRelatives(sl[1], children=True, typ="transform")
+        
+    
+    for o in listA:
+        transforms.append(pm.xform(o, q=True, matrix=True))
+    
+    
+    for o, t in zip(listB, transforms):
+        pm.xform(o, matrix=t)

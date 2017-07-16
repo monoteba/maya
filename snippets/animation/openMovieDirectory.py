1997a60f09d45b6f33eced59fe6763affba55706
Morten Andersen <mortentoo@users.noreply.github.com>
2017-07-16 16:18:08 +0200

    Update openMovieDirectory.py

--- snippets/animation/openMovieDirectory.py
+++ snippets/animation/openMovieDirectory.py
@@ -5,14 +5,14 @@ Open the "movie" directory of the current project
 
 import pymel.core as pm
 import webbrowser
+import subprocess
 import platform
 
 movieDir = pm.workspace(q=True, rootDirectory=True) + pm.workspace.fileRules['movie']
 movieDir.replace('\\', '/')
 
-
 if platform.system() == 'Darwin':
-    webbrowser.open('file://' + movieDir) # MacOS
+     subprocess.call(['open', movieDir])  # MacOS
 else:
-    webbrowser.open(movieDir) # Windows, Linux
+    webbrowser.open(movieDir)  # Windows, Linux
     

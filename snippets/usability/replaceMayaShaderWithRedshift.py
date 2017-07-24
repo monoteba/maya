5f40bd0df3c495ef2ca719633386a0f2757cd5c7
Morten Andersen <mortentoo@users.noreply.github.com>
2017-07-24 09:39:24 +0200

    Update replaceMayaShaderWithRedshift.py

--- snippets/usability/replaceMayaShaderWithRedshift.py
+++ snippets/usability/replaceMayaShaderWithRedshift.py
@@ -11,50 +11,62 @@ Finally, it assigns the Redshift material to the  objects the material was assig
 import pymel.core as pm
 import os
 
-# choose shader file to import
-shaderFilePath = pm.fileDialog2(caption='Import Redshift shader', dialogStyle=2, fileMode=1, fileFilter='Maya Files (*.ma *.mb);;Maya ASCII (*.ma);;Maya Binary (*.mb)')[0]
 
-# use filename as namespace
-filename = os.path.basename(shaderFilePath)
-namespace = os.path.splitext(filename)[0]	
+def maya2RedshiftShader():
+	# choose shader file to import
+	shaderFilePath = pm.fileDialog2(caption='Import Redshift shader', dialogStyle=2, fileMode=1, fileFilter='Maya Files (*.ma *.mb);;Maya ASCII (*.ma);;Maya Binary (*.mb)')
+
+	if not shaderFilePath:
+		return
+	
+	shaderFilePath = shaderFilePath[0]
+
+	# use filename as namespace
+	filename = os.path.basename(shaderFilePath)
+	namespace = os.path.splitext(filename)[0]	
 
 
-# get selected materials
-selectedMaterials = pm.ls(orderedSelection=True, materials=True)
+	# get selected materials
+	selectedMaterials = pm.ls(orderedSelection=True, materials=True)
 
-redshiftNodes = []
+	redshiftNodes = []
 
-# for each material...
-for material in selectedMaterials:
-	# import shader
-	nodes = pm.system.importFile(shaderFilePath, returnNewNodes=True, defaultNamespace=False, namespace=namespace)
+	# for each material...
+	for material in selectedMaterials:
+		# import shader
+		nodes = pm.system.importFile(shaderFilePath, returnNewNodes=True, defaultNamespace=False, namespace=namespace)
+		
+		# find RedshiftMaterial node in imported nodes
+		redshiftNode = None
+		for node in nodes:
+			if pm.nodeType(node) == 'RedshiftMaterial':
+				redshiftNode = node
+				redshiftNodes.append(redshiftNode)
+				break
+				
+		# get color from original material and assign it to the gammeCorrect node - or diffuse color if no gamma correct node is attached
+		colorValue = pm.getAttr(material + '.color')
 
-	# find RedshiftMaterial node in imported nodes
-	redshiftNode = None
-	for node in nodes:
-		if pm.nodeType(node) == 'RedshiftMaterial':
-			redshiftNode = node
-			redshiftNodes.append(redshiftNode)
-			break
+		# get connected node, the gammaCorrect node
+		gammaCorrectNode = pm.listConnections(redshiftNode + '.diffuse_color', destination=False, source=True)[0]
 
-	# get color from original material and assign it to the gammeCorrect node - or diffuse color if no gamma correct node is attached
-	colorValue = pm.getAttr(material + '.color')
+		if gammaCorrectNode:
+			if pm.nodeType(gammaCorrectNode) == 'gammaCorrect':
+				pm.setAttr(gammaCorrectNode + '.value', colorValue, type='double3')
+		else:
+			pm.setAttr(redshiftNode + '.diffuse_color', colorValue, type='double3')
 
-	# get connected node, the gammaCorrect node
-	gammaCorrectNode = pm.listConnections(redshiftNode + '.diffuse_color', destination=False, source=True)[0]
-
-	if gammaCorrectNode:
-		if pm.nodeType(gammaCorrectNode) == 'gammaCorrect':
-			pm.setAttr(gammaCorrectNode + '.value', colorValue, type='double3')
-	else:
-		pm.setAttr(redshiftNode + '.diffuse_color', colorValue, type='double3')
-
-	# get objects with material and assign redshift material to them
-	sg = material.shadingGroups()
-	setMembers = pm.sets(sg, q=True)
-	pm.select(setMembers, r=True)
-	pm.hyperShade(assign=redshiftNode)
-
+		# get objects with material and assign redshift material to them
+		sg = material.shadingGroups()
+		setMembers = pm.sets(sg, q=True)
+		pm.select(setMembers, r=True)
+		
+		if pm.ls(sl=True):
+		    pm.hyperShade(assign=redshiftNode)
+		    
+		
 	# select the imported redshift nodes
 	pm.select(redshiftNodes, r=True)
-    
+	    
+
+maya2RedshiftShader()

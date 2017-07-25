bf3a3c05237ed3b70fb4f5f67ac623d5c0ec91ea
Morten Andersen <mortentoo@users.noreply.github.com>
2017-07-25 14:14:12 +0200

    Create roundKeyValues.py

---
+++ snippets/animation/roundKeyValues.py
@@ -0,0 +1,13 @@
+'''
+Rounds key values of selected curves to nearest whole number
+'''
+
+import pymel.core as pm
+
+animCurves = pm.keyframe(q=True, name=True)
+
+for c in animCurves:
+	keys = pm.keyframe(c, q=True, tc=True, vc=True, sl=True)
+	for k in keys:
+	    val = round(k[1])
+	    pm.keyframe(c, e=True, t=k[0], vc=val, absolute=True)

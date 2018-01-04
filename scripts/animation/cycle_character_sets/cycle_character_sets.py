29fdda17d22efb144f92889fc344c587696364fb
Morten Andersen <11890765+mortenblaa@users.noreply.github.com>
2018-01-04 13:59:57 +0100

    Create cycle_character_sets.py

---
+++ scripts/animation/cycle_character_sets/cycle_character_sets.py
@@ -0,0 +1,33 @@
+"""
+Cycles character sets either 
+"""
+
+import pymel.core as pm
+
+def cycle_character_set(backwards=False):
+    characters = pm.ls(type="character")
+    active = pm.selectionConnection("highlightList", q=True, object=True)
+    
+    if active is None:
+        index = 0
+        
+        if backwards:
+            pm.mel.eval('setCurrentCharacters({"%s"})' % (characters[-1]))
+        else:
+            pm.mel.eval('setCurrentCharacters({"%s"})' % (characters[0]))
+            
+        return
+    
+    
+    index = characters.index(active[0])
+    new_index = 0
+    
+    new_index = (index + 1) if not backwards else (index - 1)
+    
+    if  new_index < 0 or new_index > (len(characters)-1):
+        pm.mel.eval('setCurrentCharacters({})')
+    else:
+        pm.mel.eval('setCurrentCharacters({"%s"})' % (characters[new_index]))
+    
+
+cycle_character_set(backwards=False)

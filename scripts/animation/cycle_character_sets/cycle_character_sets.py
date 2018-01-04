d380fd7936992203c9007f79dda7e53f013cbfef
Morten Andersen <11890765+mortenblaa@users.noreply.github.com>
2018-01-04 14:09:33 +0100

    Update cycle_character_sets.py

--- scripts/animation/cycle_character_sets/cycle_character_sets.py
+++ scripts/animation/cycle_character_sets/cycle_character_sets.py
@@ -8,6 +8,9 @@ def cycle_character_set(backwards=False):
     characters = pm.ls(type="character")
     active = pm.selectionConnection("highlightList", q=True, object=True)
     
+    if characters is None:
+        return
+    
     if active is None:
         index = 0
         

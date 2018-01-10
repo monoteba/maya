41db51ee3d711d61c0b3f48bdebfd621d7acee7c
Morten Andersen <11890765+mortenblaa@users.noreply.github.com>
2018-01-10 11:15:44 +0100

    Update filename_hud.py

--- scripts/utilities/headsUpDisplay/filename_hud.py
+++ scripts/utilities/headsUpDisplay/filename_hud.py
@@ -2,12 +2,19 @@ import pymel.core as pm
 import maya.api.OpenMaya as om
 import os
 
+# hud screen position
+# top:      0   1   2   3   4
+# bottom:   5   6   7   8   9
+hud_filename_section = 9
+
 hud_filename_save_callback = None
 hud_filename_open_callback = None
 
 def hud_file_object(*args):
     try:
         filename = os.path.basename(pm.system.sceneName())
+        if not filename:
+            return "(not set)"
         return filename
     except:
         return ""
@@ -21,20 +28,30 @@ def toggle_file_hud():
     
  
 def create_file_hud(event=None):
-    try:
-        pm.headsUpDisplay('HUDFilename', 
-            section=8, 
-            block=0,
-            blockSize='medium',
-            label='filename',
-            labelFontSize='small',
-            command=hud_file_object,
-            event='NewSceneOpened')
-    except RuntimeError:
-        pm.warning('HUD position is occupied!')
-        return
+    if pm.headsUpDisplay('HUDFilename', exists=True):
+        pm.headsUpDisplay('HUDFilename', rem=True)
+    
+    block = 0
+    
+    while block < 20:
+        try:
+            pm.headsUpDisplay('HUDFilename', 
+                section=9, 
+                block=block,
+                blockSize='medium',
+                label='Filename:',
+                labelFontSize='small',
+                command=hud_file_object,
+                event='NewSceneOpened')
+        except RuntimeError:
+            block = block + 1
+            continue
+        
+        break
         
     global hud_filename_save_callback
+    global hud_filename_open_callback
+    
     try:
         om.MSceneMessage.removeCallback(hud_filename_save_callback)
         om.MSceneMessage.removeCallback(hud_filename_open_callback)

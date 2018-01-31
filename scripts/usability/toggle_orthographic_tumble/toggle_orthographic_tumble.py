9a17d780481125673b2bafbc0df9c4568b5d73cf
Morten Andersen <mortenblaa@gmail.com>
2018-01-31 10:31:59 +0100

    .

--- scripts/usability/toggle_orthographic_tumble/toggle_orthographic_tumble.py
+++ scripts/usability/toggle_orthographic_tumble/toggle_orthographic_tumble.py
@@ -5,9 +5,10 @@ Toggles the orthographic locked state of the Tumble Tool
 import maya.cmds as cmds
 
 
-def toggle_orthographic_tumble():
+def toggle_orthographic_tumble(stepped=False):
     val = not (cmds.tumbleCtx('tumbleContext', q=True, orthoLock=True))
     cmds.tumbleCtx('tumbleContext', e=True, orthoLock=val)
+    cmds.tumbleCtx('tumbleContext', e=True, autoOrthoConstrain=stepped)
 
 
 toggle_orthographic_tumble()

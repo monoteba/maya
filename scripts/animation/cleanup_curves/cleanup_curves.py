e2700a4e618c1fc061cec1037f753fbdd9859308
Morten Andersen <mortenblaa@gmail.com>
2018-11-08 14:46:24 -0500

    -

--- scripts/animation/cleanup_curves/cleanup_curves.py
+++ scripts/animation/cleanup_curves/cleanup_curves.py
@@ -1,7 +1,7 @@
-'''
+"""
 Removes redundant keys on animation curves.
 
-For example, if the keyframes at time {1, 5, 9} have the same value, but at time {13} the value changes. The keyframe at time {5} will be removed. 
+For example, if the keyframes at time {1, 5, 9} have the same value, but at time {13} the value changes. The keyframe at time {5} will be removed.
 
 stepped=True
 Then both keys {5, 9} will be removed in the example above, since it would visually be identical.
@@ -11,9 +11,10 @@ Will remove the last keyframe if it is identical the the previous keyframe.
 
 tolerance=0.001
 How large the difference needs to be for two keyframes to be considered equal. Setting the value to 0 may not produce expected results.
-'''
+"""
 
 import pymel.core as pm
+import maya.mel
 
 
 def isclose(a, b, epsilon=1e-09):
@@ -25,7 +26,7 @@ def isclose(a, b, epsilon=1e-09):
     :return: True if difference between a and b is less than epsilon
     """
     return abs(a - b) < epsilon
-	
+
 
 def cleanupCurves(stepped=False, keepLast=True, tolerance=0.001):
     """
@@ -42,6 +43,15 @@ def cleanupCurves(stepped=False, keepLast=True, tolerance=0.001):
     selection = pm.ls(sl=True)
     for obj in selection:
         curves = pm.listConnections(obj, type="animCurve")
+        
+        gMainProgressBar = maya.mel.eval('$tmp = $gMainProgressBar')
+        
+        pm.progressBar(gMainProgressBar,
+                       edit=True,
+                       beginProgress=True,
+                       isInterruptable=True,
+                       status='Cleaning curves...',
+                       maxValue=len(curves))
         
         for curve in curves:
             keys = pm.keyframe(curve, q=True, timeChange=True, valueChange=True)
@@ -67,8 +77,16 @@ def cleanupCurves(stepped=False, keepLast=True, tolerance=0.001):
                 for t in toRemove:
                     pm.cutKey(curve, time=t)
                     totalKeys += 1
+            
+            # progress bar step
+            if pm.progressBar(gMainProgressBar, query=True, isCancelled=True):
+                break
+            pm.progressBar(gMainProgressBar, edit=True, step=1)
     
-    print "// {0} keys removed on ".format(totalKeys) + str([obj.name().encode() for obj in selection])
+    pm.progressBar(gMainProgressBar, edit=True, endProgress=True)
+    
+    print "# {0} keys removed on ".format(totalKeys) + str([obj.name().encode() for obj in selection])
     return int(totalKeys)
-	
-cleanupCurves(False, True, 0.001)
+
+
+cleanupCurves(True, True, 0.001)
--- scripts/usability/toggle_viewport_nurbs_curves/toggle_viewport_nurbs_curves.py
+++ scripts/usability/toggle_viewport_nurbs_curves/toggle_viewport_nurbs_curves.py
@@ -1,16 +1,16 @@
-import pymel.core as pm
+import maya.cmds as cmds
 
 
 def toggle_viewport_nurbs_curves():
-    panel = pm.getPanel(withFocus=True)
+    panel = cmds.getPanel(withFocus=True)
     
     if panel is not None:
-        panelType = pm.getPanel(typeOf=panel)
+        panelType = cmds.getPanel(typeOf=panel)
         
         # panel is model panel
         if panelType == "modelPanel" and panel:
-            nc = not pm.modelEditor(panel, q=True, nc=True)
-            pm.modelEditor(panel, e=True, nurbsCurves=nc)
+            nc = not cmds.modelEditor(panel, q=True, nc=True)
+            cmds.modelEditor(panel, e=True, nurbsCurves=nc)
 
 
 toggle_viewport_nurbs_curves()

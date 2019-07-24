3a9b51d94985a3a0435ba0e30399f7f88fd3aca5
Morten <mortenblaa@gmail.com>
2019-07-24 14:20:33 +0200

    Layout changes.

--- scripts/utilities/fix_empty_shelves/fix_empty_shelves.py
+++ scripts/utilities/fix_empty_shelves/fix_empty_shelves.py
@@ -20,13 +20,12 @@ import mmap
 import os
 import re
 import shutil
-import sys
 from time import gmtime, strftime
 
 
 def fix_empty_shelves(userprefs_file=None):
     confirm = cmds.confirmDialog(title='Fix empty shelves',
-                                 message='This will find duplicate shelf entries in userPrefs.mel.\n\n'
+                                 message='This tool will find duplicate shelf entries in userPrefs.mel.\n\n'
                                          'Maya needs to be closed at the end for the tool to work.\n\n'
                                          'Please save all work before continuing.',
                                  button=['Continue', 'Cancel'],
@@ -40,10 +39,10 @@ def fix_empty_shelves(userprefs_file=None):
     if userprefs_file is None:
         prefs_dir = cmds.about(preferences=True)
         userprefs_file = prefs_dir + '/prefs/userPrefs.mel'
-        
+    
     # convert path to platform path style
     userprefs_file = os.path.abspath(userprefs_file)
-        
+    
     if not os.path.isfile(userprefs_file):
         cmds.warning('Could not find userPrefs.mel at %s' % userprefs_file)
         return
@@ -54,11 +53,11 @@ def fix_empty_shelves(userprefs_file=None):
     
     # find duplicates
     try:
-        with open(userprefs_file, 'r+') as f:           
+        with open(userprefs_file, 'r+') as f:
             shelves = []
             indices = []
             names = []
-
+            
             data = mmap.mmap(f.fileno(), 0)
             matches = re.findall(b'-sv "shelfName([0-9]+)" "(.*?)"', data)
             data.close()
@@ -87,7 +86,8 @@ def fix_empty_shelves(userprefs_file=None):
         return
     
     # remove duplicates
-    log = 'Removed %s lines:\n' % str(len(indices))
+    log = ''
+    count = 0
     try:
         with open(userprefs_file, 'w+') as f:
             for line in lines:
@@ -96,6 +96,7 @@ def fix_empty_shelves(userprefs_file=None):
                     new_line = re.sub(pattern, '', line)
                     if line != new_line:
                         log = log + str(line)
+                        count = count + 1
                         line = new_line
                         break
                 
@@ -106,48 +107,47 @@ def fix_empty_shelves(userprefs_file=None):
     except Exception as e:
         cmds.warning('An unknown error occured when trying to write to %s (%s)' % (userprefs_file, str(e)))
         return
-
+    
+    log = 'Removed %s lines with %s duplicate shelves in file "%s":\n\n%s' % (count, len(indices), userprefs_file, log)
+    
     # fes_quit(log, len(indices))
     win = cmds.window(title='Fix Empty Shelves', rtf=True, widthHeight=(300, 100))
     form = cmds.formLayout(nd=100)
-
+    
     m = cmds.iconTextStaticLabel(st="iconAndTextHorizontal", i="info.xpm", align='center', fn='plainLabelFont',
-                                 l='Removed %d shelf duplicates in userPrefs.mel.\nPlease quit Maya to apply the changes.' % len(indices))
+                                 label='Removed %d shelf duplicates in userPrefs.mel.\n'
+                                       'Please quit Maya to apply the changes.\n'
+                                       'If you do not quit, the fixes may not be saved.' % len(indices))
     b1 = cmds.button('Quit Maya', command=lambda *args: fes_quit(win, True))
     b2 = cmds.button('View Log', command=lambda *args: fes_view_log(log))
     b3 = cmds.button('Cancel', command=lambda *args: fes_quit(win, False))
-
-    cmds.formLayout(form, e=True, numberOfDivisions=100, 
-                    attachForm=[
-                        (m, 'top', 8), (m, 'left', 8), (m, 'right', 8),
-                        (b1, 'left', 8), (b3, 'right', 8),
-                        (b1, 'bottom', 8), (b2, 'bottom', 8), (b3, 'bottom', 8)
-                        ],
-                    attachPosition=[
-                        (b1, 'right', 8, 34), (b3, 'left', 8, 66)
-                        ],
-                    attachControl=[
-                        (b2, 'left', 8, b1), (b2, 'right', 8, b3)
-                        ]
-        )
-
+    
+    cmds.formLayout(form, e=True, numberOfDivisions=100,
+                    attachForm=[(m, 'top', 8), (m, 'left', 8), (m, 'right', 8),
+                                (b1, 'left', 8), (b3, 'right', 8),
+                                (b1, 'bottom', 8), (b2, 'bottom', 8), (b3, 'bottom', 8)],
+                    attachPosition=[(b1, 'right', 8, 34), (b3, 'left', 8, 66)],
+                    attachControl=[(b2, 'left', 8, b1), (b2, 'right', 8, b3)]
+                    )
+    
     cmds.showWindow(win)
 
 
-def fes_quit(window, quit):
-    cmds.deleteUI(window, window=True)    
-
+def fes_quit(window, quit_maya):
+    cmds.deleteUI(window, window=True)
+    
     save_settings = int(cmds.optionVar(q='saveActionsPreferences'))
     cmds.optionVar(iv=('saveActionsPreferences', 0))
     
-    if quit:
+    if quit_maya:
         cmds.Quit()
     else:
         cmds.optionVar(iv=('saveActionsPreferences', save_settings))
 
+
 def fes_view_log(log):
     # show the log
-    cmds.window(title='Log', widthHeight=(350,350))
+    cmds.window(title='Log', widthHeight=(600, 450))
     cmds.paneLayout(configuration='single')
     cmds.scrollField(editable=False, wordWrap=False, text=log)
     cmds.showWindow()

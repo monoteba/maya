5a799da4679f13a87dcfc650448a1f8ad4620114
Morten Andersen <11890765+mortenblaa@users.noreply.github.com>
2019-07-24 13:57:16 +0200

    Fix for Windows.

--- scripts/utilities/fix_empty_shelves/fix_empty_shelves.py
+++ scripts/utilities/fix_empty_shelves/fix_empty_shelves.py
@@ -20,6 +20,7 @@ import mmap
 import os
 import re
 import shutil
+import sys
 from time import gmtime, strftime
 
 
@@ -39,10 +40,10 @@ def fix_empty_shelves(userprefs_file=None):
     if userprefs_file is None:
         prefs_dir = cmds.about(preferences=True)
         userprefs_file = prefs_dir + '/prefs/userPrefs.mel'
-    
-    # replace backslashes with forward slashes
-    userprefs_file = userprefs_file.replace('\\', '/')
-    
+        
+    # convert path to platform path style
+    userprefs_file = os.path.abspath(userprefs_file)
+        
     if not os.path.isfile(userprefs_file):
         cmds.warning('Could not find userPrefs.mel at %s' % userprefs_file)
         return
@@ -52,77 +53,101 @@ def fix_empty_shelves(userprefs_file=None):
     shutil.copy2(userprefs_file, backup_file)
     
     # find duplicates
-    if os.access(userprefs_file, os.R_OK):
-        try:
-            with open(userprefs_file, 'r+') as f:
-                data = mmap.mmap(f.fileno(), 0)
-                matches = re.findall(b'-sv "shelfName([0-9]+)" "(.*?)"', data)
-                
-                shelves = []
-                indices = []
-                names = []
-                
-                for shelf in matches:
-                    if shelf[1] not in shelves:
-                        shelves.append(shelf[1])
-                    else:
-                        names.append(shelf[1])
-                        indices.append(shelf[0])
-                
-                f.seek(0)
-                lines = f.readlines()
-        except IOError as e:
-            cmds.warning('Could not read from file %s (%s)' % (userprefs_file, str(e)))
-            return
-        except Exception as e:
-            cmds.warning('An unknown error occured when trying to read from %s (%s)' % (userprefs_file, str(e)))
-            return
-    else:
-        cmds.warning('Failed! Does not have permission to read file %s' % userprefs_file)
+    try:
+        with open(userprefs_file, 'r+') as f:           
+            shelves = []
+            indices = []
+            names = []
+
+            data = mmap.mmap(f.fileno(), 0)
+            matches = re.findall(b'-sv "shelfName([0-9]+)" "(.*?)"', data)
+            data.close()
+            
+            for shelf in matches:
+                if shelf[1] not in shelves:
+                    shelves.append(shelf[1])
+                else:
+                    names.append(shelf[1])
+                    indices.append(shelf[0])
+            
+            f.seek(0, 0)
+            lines = f.readlines()
+    except IOError as e:
+        cmds.warning('Could not read from file %s (%s)' % (userprefs_file, str(e)))
+        return
+    except Exception as e:
+        cmds.warning('An unknown error occured when trying to read from %s (%s)' % (userprefs_file, str(e)))
         return
     
     if not indices:
-        cmds.confirmDialog(title='No duplicates found :/',
+        cmds.confirmDialog(title='No duplicates found',
                            message='No changed were done and you can safely close the tool.',
                            button=['OK'], cancelButton='OK', dismissString='OK')
         os.remove(backup_file)
         return
     
     # remove duplicates
-    if os.access(userprefs_file, os.W_OK):
-        try:
-            with open(userprefs_file, 'w+') as f:
-                for line in lines:
-                    for i in indices:
-                        pattern = re.compile('-.. "shelf(.*?)%s" (".*?"|[0-9+])' % i)
-                        new_line = re.sub(pattern, '', line)
-                        if line != new_line:
-                            line = new_line
-                            break
-                    
-                    f.writelines(line)
-        except IOError as e:
-            cmds.warning('IOError: Could not write to file %s (%s)' % (userprefs_file, str(e)))
-            return
-        except Exception as e:
-            cmds.warning('An unknown error occured when trying to write to %s (%s)' % (userprefs_file, str(e)))
-            return
-    else:
-        cmds.warning('Failed: Does not have permission to write to file %s' % userprefs_file)
+    log = 'Removed %s lines:\n' % str(len(indices))
+    try:
+        with open(userprefs_file, 'w+') as f:
+            for line in lines:
+                for i in indices:
+                    pattern = re.compile('-.. "shelf(.*?)%s" (".*?"|[0-9+])' % i)
+                    new_line = re.sub(pattern, '', line)
+                    if line != new_line:
+                        log = log + str(line)
+                        line = new_line
+                        break
+                
+                f.writelines(line)
+    except IOError as e:
+        cmds.warning('IOError: Could not write to file %s (%s)' % (userprefs_file, str(e)))
         return
-    
-    # restart maya
-    restart = cmds.confirmDialog(title='Complete!',
-                                 message='Removed %d shelf duplicates in userPrefs.mel.\n'
-                                         'Please restart Maya for the changes to take effect.' % len(indices),
-                                 button=['Quit Maya', 'Cancel'],
-                                 cancelButton='Cancel',
-                                 dismissString='Cancel')
-    
+    except Exception as e:
+        cmds.warning('An unknown error occured when trying to write to %s (%s)' % (userprefs_file, str(e)))
+        return
+
+    # fes_quit(log, len(indices))
+    win = cmds.window(title='Fix Empty Shelves', rtf=True, widthHeight=(300, 100))
+    form = cmds.formLayout(nd=100)
+
+    m = cmds.iconTextStaticLabel(st="iconAndTextHorizontal", i="info.xpm", align='center', fn='plainLabelFont',
+                                 l='Removed %d shelf duplicates in userPrefs.mel.\nPlease quit Maya to apply the changes.' % len(indices))
+    b1 = cmds.button('Quit Maya', command=lambda *args: fes_quit(win, True))
+    b2 = cmds.button('View Log', command=lambda *args: fes_view_log(log))
+    b3 = cmds.button('Cancel', command=lambda *args: fes_quit(win, False))
+
+    cmds.formLayout(form, e=True, numberOfDivisions=100, 
+                    attachForm=[
+                        (m, 'top', 8), (m, 'left', 8), (m, 'right', 8),
+                        (b1, 'left', 8), (b3, 'right', 8),
+                        (b1, 'bottom', 8), (b2, 'bottom', 8), (b3, 'bottom', 8)
+                        ],
+                    attachPosition=[
+                        (b1, 'right', 8, 34), (b3, 'left', 8, 66)
+                        ],
+                    attachControl=[
+                        (b2, 'left', 8, b1), (b2, 'right', 8, b3)
+                        ]
+        )
+
+    cmds.showWindow(win)
+
+
+def fes_quit(window, quit):
+    cmds.deleteUI(window, window=True)    
+
     save_settings = int(cmds.optionVar(q='saveActionsPreferences'))
     cmds.optionVar(iv=('saveActionsPreferences', 0))
     
-    if restart == 'Quit Maya':
+    if quit:
         cmds.Quit()
     else:
         cmds.optionVar(iv=('saveActionsPreferences', save_settings))
+
+def fes_view_log(log):
+    # show the log
+    cmds.window(title='Log', widthHeight=(350,350))
+    cmds.paneLayout(configuration='single')
+    cmds.scrollField(editable=False, wordWrap=False, text=log)
+    cmds.showWindow()
